93feb677f5309dc4b4150bdea83c6ca62271f951
vincent <vincent.arteaga@gmail.com>
2021-08-05 20:18:23 -0700

    commit tree 43323a98737f46dd181b1e68759003d946ea8d68

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :roster
+    # init with roster
+    def initialize(roster)
+        @roster = {}
+
+        
+    end
+    # add student method
+    def add_student(student, grade)
+        # uses the or equals operator
+        @roster[grade] ||= []
+        @roster[grade] << student
+        
+    end
+    # method to retrieve students from a grade
+    def grade(grade)
+        @roster[grade]        
+    end
+    # sorts student
+    def sort
+        @roster.sort_by {|k, v| v}.to_h
+    end
+end
